03cd9a94980b7ec451937c655d9f934fd152613e
Prakhar Srivastav <prakhar1989@gmail.com>
2018-11-30 22:20:51 -0800

    minor changes

--- src/day01.rs
+++ src/day01.rs
@@ -2,26 +2,28 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input/day01.txt");
-    let total = part1(input);
-    println!("Total: {}", total);
-    println!("already seen: {}", part2(input));
-
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn to_nums(input: &str) -> Vec<i32> {
-    let xs: Vec<&str>  = input.split("\n")
+    let terms: Vec<&str>  = input.split("\n")
         .map(|s: &str| s.trim())
         .filter(|x| !x.is_empty())
         .collect();
 
-    let mut entries = vec![];
-    for x in &xs {
+    let mut nums = vec![];
+    for x in &terms {
         let multiplier = if x.starts_with("-") { -1 } else { 1 };
-        let rest = &x[1..];
-        entries.push(multiplier * rest.parse::<i32>().unwrap());
+        match &x[1..].parse::<i32>() {
+            Ok(v) => {
+                nums.push(multiplier * v);
+            }
+            Err(_e) => continue
+        }
     }
 
-    entries
+    nums
 }
 
 fn part1(input: &str) -> i32 {
@@ -52,10 +54,6 @@ mod tests {
     #[test]
     fn day01() {
         assert_eq!(part1("-4\n+5\n+10"), 11);
-    }
-
-    #[test]
-    fn day01_part_two() {
         assert_eq!(part2("-6\n,+3\n,+8\n,+5\n,-6"), 5);
         assert_eq!(part2("+3\n,+3\n,+4\n,-2\n,-4"), 10);
     }
