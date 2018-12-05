7734ff6bf0ab883a2e30d4467145edd2e0cb267c
Prakhar Srivastav <prakhar1989@gmail.com>
2018-12-04 23:22:34 -0800

    simpler check for can react

--- src/day05.rs
+++ src/day05.rs
@@ -2,18 +2,18 @@ const INPUT: &str = include_str!("../input/day05.txt");
 
 fn main() {
     let input = INPUT.trim();
-    println!("Part 1: {}", part1(input).len());
+    println!("Part 1: {}", part1(input));
     println!("Part 2: {:?}", part2(input));
 }
 
-fn part1(input: &str) -> String {
+fn part1(input: &str) -> usize {
     let mut stack = Vec::new();
     for c in input.chars() {
         if stack.is_empty() {
             stack.push(c);
         } else {
             if let Some(v) = stack.pop() {
-                if !can_react(v, c) {
+                if !(v != c && c.to_ascii_uppercase() == v.to_ascii_uppercase()) {
                     stack.push(v);
                     stack.push(c);
                 }
@@ -21,12 +21,7 @@ fn part1(input: &str) -> String {
         }
     }
 
-    stack.iter().collect()
-}
-
-fn can_react(c1: char, c2: char) -> bool {
-    let diff: i32 = (c1 as i32) - (c2 as i32); // since ascii
-    diff.abs() == 32
+    stack.iter().collect::<String>().len()
 }
 
 fn part2(input: &str) -> Option<usize> {
@@ -37,7 +32,7 @@ fn part2(input: &str) -> Option<usize> {
                 .chars()
                 .filter(|x| *x != c && *x != c.to_ascii_uppercase())
                 .collect::<String>();
-            part1(&reacted).len()
+            part1(&reacted)
         }).min()
 }
 
@@ -46,19 +41,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day05_can_react() {
-        assert_eq!(can_react('a', 'A'), true);
-        assert_eq!(can_react('A', 'a'), true);
-        assert_eq!(can_react('a', 'b'), false);
-    }
-
-    #[test]
     fn day05_part1() {
-        assert_eq!(part1("aA"), "");
-        assert_eq!(part1("abBA"), "");
-        assert_eq!(part1("abAB"), "abAB");
-        assert_eq!(part1("aabAAB"), "aabAAB");
-        assert_eq!(part1("dabAcCaCBAcCcaDA"), "dabCBAcaDA");
+        assert_eq!(part1("aA"), 0);
+        assert_eq!(part1("abBA"), 0);
+        assert_eq!(part1("abAB"), 4);
+        assert_eq!(part1("aabAAB"), 6);
+        assert_eq!(part1("dabAcCaCBAcCcaDA"), 10);
     }
 
     #[test]
