c1513a80d5df7fdbcccf3dd9de6c4927cfc3b1e7
Prakhar Srivastav <prakhar1989@gmail.com>
2018-12-11 16:55:55 -0800

    use summed area tables for day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,21 +1,22 @@
 mod matrix;
 
 type Point = (usize, usize);
+type Grid = matrix::Matrix<i32>;
 
 fn main() {
     let serial_number = 6392;
-    let m = build_grid(serial_number);
-    println!("Part 1: {:?}", find_fuel_cell(&m, 3).0);
-    println!("Part 2: {:?}", largest_cell(&m));
+    let area_table = summed_area_table(&build_grid(serial_number));
+    println!("Part 1: {:?}", find_fuel_cell(&area_table, 3).0);
+    println!("Part 2: {:?}", largest_cell(&area_table));
 }
 
-fn largest_cell(m: &matrix::Matrix<i32>) -> (Point, usize) {
+fn largest_cell(area_table: &Grid) -> (Point, usize) {
     let mut best_size = 1;
     let mut best_point = (0, 0);
     let mut max_power = 0;
 
     for s in 1..300 {
-        let (point, power) = find_fuel_cell(m, s);
+        let (point, power) = find_fuel_cell(area_table, s);
         if power > max_power {
             best_size = s;
             best_point = point;
@@ -26,13 +27,38 @@ fn largest_cell(m: &matrix::Matrix<i32>) -> (Point, usize) {
     (best_point, best_size)
 }
 
-fn find_fuel_cell(m: &matrix::Matrix<i32>, size: usize) -> (Point, i32) {
+/// converts a table to partial sum as per the algo described here:
+/// https://en.wikipedia.org/wiki/Summed-area_table
+fn summed_area_table(m: &Grid) -> Grid {
+    let mut table = matrix::Matrix::new(m.height, m.width, 0);
+
+    table[(0, 0)] = m[(0, 0)];
+
+    for i in 1..table.width {
+        table[(i, 0)] = m[(i, 0)] + table[(i - 1, 0)];
+    }
+
+    for j in 1..table.height {
+        table[(0, j)] = m[(0, j)] + table[(0, j - 1)];
+    }
+
+    for i in 1..table.width {
+        for j in 1..table.height {
+            table[(i, j)] =
+                m[(i, j)] + table[(i - 1, j)] + table[(i, j - 1)] - table[(i - 1, j - 1)];
+        }
+    }
+
+    table
+}
+
+fn find_fuel_cell(area_table: &Grid, size: usize) -> (Point, i32) {
     let mut max_power = 0;
     let mut max_cell = (0, 0);
 
-    for i in 0..(300-size) {
-        for j in 0..(300-size) {
-            let power = total_power((i, j), &m, size);
+    for i in 0..(300 - size) {
+        for j in 0..(300 - size) {
+            let power = total_power((i, j), &area_table, size);
             if power > max_power {
                 max_cell = (i, j);
                 max_power = power;
@@ -43,7 +69,7 @@ fn find_fuel_cell(m: &matrix::Matrix<i32>, size: usize) -> (Point, i32) {
     (max_cell, max_power)
 }
 
-fn build_grid(serial_number: i32) -> matrix::Matrix<i32> {
+fn build_grid(serial_number: i32) -> Grid {
     let mut m = matrix::Matrix::new(300, 300, 0);
 
     for i in 0..300 {
@@ -55,16 +81,31 @@ fn build_grid(serial_number: i32) -> matrix::Matrix<i32> {
     m
 }
 
-fn total_power(point: Point, grid: &matrix::Matrix<i32>, size: usize) -> i32 {
-    let mut area = 0;
+/// Calculates the total area of a summed area table as per formula in
+/// https://en.wikipedia.org/wiki/Summed-area_table
+/// The variable names A, B, C, D correspond to the algorithm shown above
+fn total_power(point: Point, summed_table: &Grid, size: usize) -> i32 {
+    let (i, j) = point;
+    let d = summed_table[(i + size - 1, j + size - 1)];
+    let a = if (i as isize) - 1 >= 0 && (j as isize) - 1 >= 0 {
+        summed_table[(i - 1, j - 1)]
+    } else {
+        0
+    };
 
-    for i in 0..size {
-        for j in 0..size {
-            area += grid[(point.0 + i, point.1 + j)];
-        }
-    }
+    let b = if (j as isize) - 1 >= 0 {
+        summed_table[(i + size - 1, j - 1)]
+    } else {
+        0
+    };
 
-    area
+    let c = if (i as isize) - 1 >= 0 {
+        summed_table[(i - 1, j + size - 1)]
+    } else {
+        0
+    };
+
+    d + a - b - c
 }
 
 fn power_level(point: Point, serial_number: i32) -> i32 {
@@ -72,10 +113,10 @@ fn power_level(point: Point, serial_number: i32) -> i32 {
     let rack_id = (x as i32) + 10;
     let j = ((rack_id * y as i32) + serial_number) * rack_id;
 
-    hundered_digit(j) - 5
+    hundredth_digit(j) - 5
 }
 
-fn hundered_digit(n: i32) -> i32 {
+fn hundredth_digit(n: i32) -> i32 {
     ((n - (n % 100)) / 100) % 10
 }
 
@@ -105,13 +146,33 @@ mod tests {
 
     #[test]
     fn day11_total_power() {
-        let m = build_grid(18);
-        assert_eq!(total_power((33, 45), &m, 3), 29);
+        let area_table = summed_area_table(&build_grid(18));
+        assert_eq!(total_power((33, 45), &area_table, 3), 29);
     }
 
     #[test]
     fn day11_find_fuel_cell() {
-        let m = build_grid(6392);
-        assert_eq!(find_fuel_cell(&m, 3).0, (20, 58));
+        let area_table = summed_area_table(&build_grid(6392));
+        assert_eq!(find_fuel_cell(&area_table, 3).0, (20, 58));
+    }
+
+    #[test]
+    fn day11_summed_area_table() {
+        let mut m = matrix::Matrix::new(3, 3, 0);
+        let v = vec![5, 2, 3, 1, 5, 4, 2, 2, 1];
+
+        for i in 0..3 {
+            for j in 0..3 {
+                m[(i, j)] = v[i * 3 + j];
+            }
+        }
+
+        let summed_table = summed_area_table(&m);
+        assert_eq!(
+            format!("{:?}", summed_table),
+            "[[5, 7, 10]
+ [6, 13, 20]
+ [8, 17, 25]]"
+        );
     }
 }
